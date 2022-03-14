4a30e72575eb31b14e2e2063596601ad5d0fc340
surendra <shresthasurendra37@gmail.com>
2022-03-14 23:33:06 +0545

    adding tekmetric

---
+++ JetComSmsSync/Modules/JetComSMSSync.Modules.ShopWare/ShopWareModule.cs
@@ -0,0 +1,20 @@
+﻿using JetComSMSSync.Modules.ShopWare.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace JetComSMSSync.Modules.ShopWare
+{
+    public class ShopWareModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
